f49818408d7a2024caa6b096c45cc0b9aecc2d51
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-09 14:37:35 +0100

    configuire jshint in jake

--- jakefile.js
+++ jakefile.js
@@ -3,5 +3,12 @@ task("default", ["lint"]);
 desc("Lint everything");
 task("lint", [], function() {
    var lint = require("./build/lint/lint_runner.js");
-   lint.validateFile("jakefile.js", {});
+   var files = new jake.FileList();
+   files.include("**/*.js");
+   files.exclude("node_modules");
+   files.exclude("build");
+   var options = {
+      node: true
+   };
+   lint.validateFileList(files.toArray(), {});
 });
