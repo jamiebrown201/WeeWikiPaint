812fcb780eec8a32a4ed11834e16d95e603bc485
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-10 16:06:34 +0100

    added continous integration script

--- jakefile.js
+++ jakefile.js
@@ -1,36 +1,53 @@
+/*global desc, task, jake, fail, complete */
 (function() {
-   "use strict";
+    "use strict";
 
-   task("default", ["lint"]);
+    desc("Build and test");
+    task("default", ["lint"]);
 
-   desc("Lint everything");
-   task("lint", [], function() {
-      var lint = require("./build/lint/lint_runner.js");
+    function nodeLintOptions() {
+        return {
+            bitwise:true,
+            curly:false,
+            eqeqeq:true,
+            forin:true,
+            immed:true,
+            latedef:true,
+            newcap:true,
+            noarg:true,
+            noempty:true,
+            nonew:true,
+            regexp:true,
+            undef:true,
+            strict:true,
+            trailing:true,
+            node:true
+        };
+    }
 
-      var files = new jake.FileList();
-      files.include("**/*.js");
-      files.exclude("node_modules");
-      var options = nodeLintOptions();
-      lint.validateFileList(files.toArray(), options, {});
-   });
+    desc("Lint everything");
+    task("lint", [], function() {
+        var lint = require("./build/lint/lint_runner.js");
 
-   function nodeLintOptions() {
-      return {
-         bitwise:true,
-         curly:false,
-         eqeqeq:true,
-         forin:true,
-         immed:true,
-         latedef:true,
-         newcap:true,
-         noarg:true,
-         noempty:true,
-         nonew:true,
-         regexp:true,
-         undef:true,
-         strict:true,
-         trailing:true,
-         node:true
-      };
-   }
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+        var passed = lint.validateFileList(files.toArray(), options, {});
+        if (!passed) fail("Lint failed");
+    });
+
+    desc("Integrate");
+    task("integrate", ["default"], function() {
+        console.log("1. Make sure 'git status' is clean.");
+        console.log("2. Build on the integration box.");
+        console.log("   a. Walk over to integration box.");
+        console.log("   b. 'git pull'");
+        console.log("   c. 'jake'");
+        console.log("   d. If jake fails, stop! Try again after fixing the issue.");
+        console.log("3. 'git checkout integration'");
+        console.log("4. 'git merge master --no-ff --log'");
+        console.log("5. 'git checkout master'");
+    });
+
 }());
